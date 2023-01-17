05ca4f4f50769dc07bddbecb3ca4e071e9a7e6fb
joeleagles <121190800+joeleagles@users.noreply.github.com>
2023-01-17 12:04:37 -0600

    Update Step1-Export.bat

--- Chrome-profile-move/Step1-Export.bat
+++ Chrome-profile-move/Step1-Export.bat
@@ -1,4 +1,43 @@
 @echo off
+
+REM --> Script Made By Joel Eagles
+
+
+
+
+REM  --> this will run the script as admin (stole this section from someone online)
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------  
+
+
+
 Xcopy "%LOCALAPPDATA%\Google\Chrome\User Data\Default" "%~dp0"Default\ /E /I /Q
 
 echo you will need to export passwords manually by going to chrome://settings/passwords and exporting to this location
